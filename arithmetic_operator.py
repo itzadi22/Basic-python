674601b910eca18d50faa570d56a8987790c833b
Adarsh siddarushi <72485050+itzadi22@users.noreply.github.com>
2021-03-22 10:23:04 +0530

    Add files via upload

---
+++ arithmetic_operator.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 19 13:29:27 2021
+
+@author: ASUS
+
+Operators
+1.Arthmetic operators
+a.+
+b.-
+c.*
+d./
+e.%
+f.**
+g.//
+
+"""
+print(2+9)
+
+print("itz" + "adi")
+
+print(99-999)
+
+print(99*19)
+
+print(25/5)
+
+print(25//5)
+
+print(8%2)
+
+print(8%3)
+
+print(15**2)
+print(15**15)
---
+++ assignment_operator.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 21 17:46:14 2021
+
+Assignment(assigning a value) Operators
+
+1.=
+2.+=
+3.-=
+
+@author: ASUS
+"""
+a=1
+a+=5
+print(a)
+a-=2
+print(a)
+
+a*=2
+print(a)
+
+a**=3
+print(a)
+
+a/=2
+print(a)
+
+a//=2
+print(a)
+
+a%=4
+print(a)
+
